cd9dd2437bfb4ad78e5c860e941b033521c50c12
Youngbin Han <ybhan@ubuntu.com>
2023-03-29 18:07:55 +0900

    Add Account theme template

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,30 @@
 import { createRoot } from "react-dom/client";
-import { StrictMode } from "react";
-import { kcContext } from "./kcContext";
-import KcApp from "./KcApp";
+import { StrictMode, lazy, Suspense } from "react";
+import { kcContext as kcLoginThemeContext } from "./login/kcContext";
+import { kcContext as kcAccountThemeContext } from "./account/kcContext";
 
-if( kcContext === undefined ){
-    throw new Error(
-        "This app is a Keycloak theme" +
-        "It isn't meant to be deployed outside of Keycloak"
-    );
-}
+const KcLoginThemeApp = lazy(() => import("./login/KcApp"));
+const KcAccountThemeApp = lazy(() => import("./account/KcApp"));
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <KcApp kcContext={kcContext} />
+        <Suspense>
+            {(()=>{
+
+                if( kcLoginThemeContext !== undefined ){
+                    return <KcLoginThemeApp kcContext={kcLoginThemeContext} />;
+                }
+
+                if( kcAccountThemeContext !== undefined ){
+                    return <KcAccountThemeApp kcContext={kcAccountThemeContext} />;
+                }
+
+                throw new Error(
+                  "This app is a Keycloak theme" +
+                  "It isn't meant to be deployed outside of Keycloak"
+                );
+
+            })()}
+        </Suspense>
     </StrictMode>
 );
